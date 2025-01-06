766bce1a89f6033b78d70e4ce93ec1a605e8e048
Anton Hughes <kurrent93@gmail.com>
2025-01-06 13:18:09 +1300

    Add AxumTemplate struct and related data structures for OpenAPI integration

---
+++ src/lib.rs
@@ -0,0 +1,105 @@
+use askama::Template;
+use openapiv3::OpenAPI;
+use serde::Serialize;
+
+#[derive(Template)]
+#[template(path = "axum.rs.jinja")]
+pub struct AxumTemplate<'a> {
+    pub openapi: &'a OpenAPI,
+    pub routes: Vec<Route>,
+    pub schemas: Vec<Schema>,
+}
+
+#[derive(Serialize)]
+pub struct Route {
+    pub path: String,
+    pub method: String,
+    pub handler_name: String,
+    pub parameters: Vec<Parameter>,
+    pub responses: Vec<Response>,
+}
+
+#[derive(Serialize)]
+pub struct Parameter {
+    pub name: String,
+    pub param_type: String,
+    pub required: bool,
+}
+
+#[derive(Serialize)]
+pub struct Response {
+    pub status_code: String,
+    pub description: String,
+    pub content_type: String,
+}
+
+#[derive(Serialize)]
+pub struct Schema {
+    pub name: String,
+    pub fields: Vec<SchemaField>,
+}
+
+#[derive(Serialize)]
+pub struct SchemaField {
+    pub name: String,
+    pub field_type: String,
+    pub required: bool,
+}
+
+impl AxumTemplate<'_> {
+    pub fn from_openapi(openapi: &OpenAPI) -> Self {
+        let routes = openapi.paths.iter()
+            .flat_map(|(path, item)| {
+                item.operations.iter()
+                    .map(|(method, operation)| Route {
+                        path: path.clone(),
+                        method: method.to_string().to_uppercase(),
+                        handler_name: format!("handle_{}_{}", method, path.replace('/', "_").trim_matches('_')),
+                        parameters: operation.parameters.iter()
+                            .filter_map(|param_ref| {
+                                param_ref.as_ref().ok().map(|param| Parameter {
+                                    name: param.name.clone(),
+                                    param_type: param.schema.as_ref().map_or("String".to_string(), |s| s.to_string()),
+                                    required: param.required,
+                                })
+                            })
+                            .collect(),
+                        responses: operation.responses.responses.iter()
+                            .map(|(status_code, response)| Response {
+                                status_code: status_code.to_string(),
+                                description: response.description.clone().unwrap_or_default(),
+                                content_type: response.content.keys().next().cloned().unwrap_or_default(),
+                            })
+                            .collect(),
+                    })
+            })
+            .collect();
+
+        let schemas = openapi.components.as_ref()
+            .map_or(Vec::new(), |components| {
+                components.schemas.iter()
+                    .map(|(name, schema_ref)| {
+                        let schema = schema_ref.as_ref().ok()?;
+                        let fields = schema.properties.iter()
+                            .map(|(field_name, field_schema)| SchemaField {
+                                name: field_name.clone(),
+                                field_type: field_schema.to_string(),
+                                required: schema.required.contains(field_name),
+                            })
+                            .collect();
+                        Some(Schema {
+                            name: name.clone(),
+                            fields,
+                        })
+                    })
+                    .filter_map(|x| x)
+                    .collect()
+            });
+
+        AxumTemplate {
+            openapi,
+            routes,
+            schemas,
+        }
+    }
+}
